4fbfcf2e074034ad11069d98ca59b5420f1be352
Hideki515 <61321130+Hideki515@users.noreply.github.com>
2023-03-22 13:34:38 -0300

    fibonacci correto

--- Fibonacci.c
+++ Fibonacci.c
@@ -3,26 +3,48 @@
 #include <math.h>
 #include <locale.h>
 
+int fibonacci(int n){
+    int a = 0, b = 1;
+
+    if (n == a || n == b)
+    {
+        return 1;//Pertence a sequencia fibonacci
+    }
+
+    //Sequencia fibonacci
+    while(b < n){
+        int auxiliar = b;
+        b = a + b;
+        a = auxiliar;
+
+        if (b == n)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
+}
+
+
 int main() {
 
     setlocale(LC_ALL, "Portuguese");
 
-    int a, b, auxiliar, i, n;
-
-    a = 0;
-    b = 1;
-
-    printf("Digite um número: ");
-    scanf("%d", &n);
-    printf("\nSérie de Fibonacci:\n\n");
-    printf("%d\n", b);
-
-    for(i = 0; i < n; i++) {
-
-        auxiliar = a + b;
-        a = b;
-        b = auxiliar;
-
-        printf("%d\n", auxiliar);
+    int num;
+    printf("Digite um numero: ");
+    fflush(stdin);
+    scanf("%d", &num);
+    
+    if (fibonacci(num))
+    {
+        printf("O numero %d faz parte da sequencia fibonacci.\n", num);
+    } else
+    {
+        printf("O numero %d nao pertence a sequencia fibonacci.\n", num);
     }
+    
+    return 0;
+
+
 }
